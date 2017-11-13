8f8a33843a408dde15b51662c322b60423f25f4f
raj2611 <sumant261195@gmail.com>
2017-11-13 15:43:15 +0530

    Update 5a.pl

--- 5a.pl
+++ 5a.pl
@@ -1,9 +1,13 @@
 #!/usr/bin/perl
 use CGI':standard';
-print "Content-type:text/html\n\n";
-foreach $i(sort keys %ENV)
-{
-    print "$i : $ENV{$i} <br/>"
-}
-
-
+print "content-type:text/html","\n\n";
+print "<html>\n";
+print "<head><title>about this serever</title></head>","\n";
+print "<hr>";
+print "server name:",$ENV{'SERVER_NAME'},"<br>";
+print "running on port:",$ENV{'SERVER_PORT'},"<br>";
+print "server software :",$ENV{'SERVER_SOFTWARE'},"<br>";
+print "CGI-Version:",$ENV{'GATEWAY_INTERFACE'},"<br>";
+print "<hr>\n";
+print "</body></html>\n";
+exit(0);
